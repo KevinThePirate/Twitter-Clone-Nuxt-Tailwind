a98805c639b40ce1575f3c01598441cc522a3304
Kevin <smithkevin1100@gmail.com>
2024-05-04 12:58:14 +0100

    Set up Left Sidebar Frontend

---
+++ components/Sidebar/Right/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col">
+    <!-- Preview Card: What's Happening  -->
+    <div>Asd</div>
+
+    <!-- Preview Card: Who to Follow  -->
+    <div>Asd</div>
+  </div>
+</template>
+
+<script setup></script>
