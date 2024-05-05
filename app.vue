b575faefe2a33e2877113490548f72772ea32017
Kevin <smithkevin1100@gmail.com>
2024-05-05 14:11:52 +0100

    began main content

--- app.vue
+++ app.vue
@@ -12,8 +12,8 @@
           </div>
 
           <!-- Main Content -->
-          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
-            <h1>Main Content</h1>
+          <main class="col-span-12 md:col-span-8 xl:col-span-6">
+            <router-view />
           </main>
 
           <!-- Right Side Bar -->
